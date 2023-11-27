4299dad4da0a989d82f0e9581b16c6b62141c9ca
Omri <omriluz1994@gmail.com>
2023-11-27 23:16:52 +0200

    add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://bestpassgen.com",
-  integrations: [tailwind(), react(), sitemap()]
+  integrations: [
+    tailwind(),
+    react(),
+    sitemap(),
+    partytown({
+      config: {
+        forward: ["datalayer.push"]
+      }
+    })]
 });
